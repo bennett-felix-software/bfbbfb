d84bf5da4bcdbd81279e8b789ce6185f4caa1931
Felix Prasanna <fpx@mit.edu>
2025-01-09 15:01:16 -0500

    add smoke test

--- tests.py
+++ tests.py
@@ -1,7 +1,25 @@
-import generate
+from generate import Interpreter, ADD, MOV
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+def test_smoke():
+    i = Interpreter()
+    i.exec(
+        [
+            ADD(69),
+            MOV(0, 1),
+        ]
+    )
+    assert i.tape[0] == 0
+    assert i.tape[1] == 69
