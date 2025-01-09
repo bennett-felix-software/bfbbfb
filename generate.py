795666b65f2e00ebb2484137de6cb31028f0d0b5
Felix Prasanna <fpx@mit.edu>
2025-01-09 12:53:19 -0500

    init

---
+++ generate.py
@@ -0,0 +1,22 @@
+DP = "r12"
+IP = "r13"
+SCRATCH = "r14"
+
+
+def emit_move_left():
+    # Saturating sub
+    print(f"xor {SCRATCH}, {SCRATCH}")
+    print(f"sub {IP}, 1")
+    print(f"cmovae {IP}, {SCRATCH}")
+
+
+def emit_move_right():
+    print(f"add {IP}, 1")
+
+
+def increment():
+    print(f"inc byte ptr [{DP}]")
+
+
+def decrement():
+    print(f"dec byte ptr [{DP}]")
